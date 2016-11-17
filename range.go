5e31ff3f2ca2a5e47d43c596b52f047be6dc7997
George Zalepa <george@purity.io>
2016-11-17 13:13:11 -0500

    Finish Range

---
+++ range.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// here we use range to sum numbers in a slice
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("Sum:", sum)
+
+	// range on arrays and slices provides both the index and
+	// value for each entry. Above we didn't need the index so
+	// we ignored it with the blank identifier `_`. Sometimes
+	// we actually want the indexes though
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	// `range` on map iterates over key/value pairs
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	// `range` on strings iterates over Unicode code points. The
+	// first value is the starting byte index of the `rune` and the
+	// second the `rune` itself
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
+}
